f5f97af8e7ae81fd771043d10b4954a05dbf7d8f
CraZZy67 <sergei89066026767@gmail.com>
2024-05-10 15:29:49 +0300

    Добавил функционал для кнопки добавить канал

---
+++ fsm.py
@@ -0,0 +1,23 @@
+import json
+
+from aiogram.fsm.state import StatesGroup, State
+
+
+class AddState(StatesGroup):
+    id = State()
+
+    @staticmethod
+    def add_json_channels(id_: int, full_name: str):
+        with open("channels.json", "r", encoding="utf-8") as file:
+            len_ = len(json.loads(file.read()))
+
+        if len_ != 0:
+            with open("channels.json", "r", encoding="utf-8") as file:
+                update_obj = json.loads(file.read())
+                update_obj[f"{len_ + 1}"] = {"full_name": full_name, "id": id_}
+
+        with open("channels.json", "w", encoding="utf-8") as file:
+            if len_ == 0:
+                json.dump({f"{len_ + 1}": {"full_name": full_name, "id": id_}}, file, indent=4)
+            else:
+                json.dump(update_obj, file, indent=4)
--- hand_admin_side.py
+++ hand_admin_side.py
@@ -1,10 +1,12 @@
-from aiogram.types import Message, CallbackQuery
-from aiogram import Router, F
+from aiogram.types import Message, CallbackQuery, ChatFullInfo
+from aiogram import Router, F, Bot
+from aiogram.fsm.context import FSMContext
 
 import json
 
 from keyboard import kb_admin, kb_channels_list
 from support_func import pars_json_to_list
+from fsm import AddState
 
 router_for_admin = Router()
 valid_users = [5617141084, 1162899410]
@@ -28,5 +30,23 @@ async def handling_channels_list(callback: CallbackQuery):
 
 
 @router_for_admin.callback_query(F.data == "back_to_menu")
-async def handling_channels_list(callback: CallbackQuery):
+async def handling_back_to_menu(callback: CallbackQuery):
     await callback.message.edit_text("Выберите действие.", reply_markup=kb_admin())
+
+
+@router_for_admin.callback_query(F.data == "add_channel")
+async def handling_first_state(callback: CallbackQuery, state: FSMContext):
+    await state.set_state(AddState.id)
+    await callback.message.answer("Перешлите любой пост из канала который хотите добавить.")
+    await callback.message.answer("И не забудьте добавит бота в администраторы канала!")
+    await callback.answer()
+
+
+@router_for_admin.message(AddState.id)
+async def catch_id_state(message: Message, state: FSMContext, bot: Bot):
+    data = await state.update_data(id=message.forward_origin.chat.id)
+    chat = message.forward_origin.chat.username
+    AddState.add_json_channels(data["id"], chat)
+    await state.clear()
+    await message.answer("Канал добавлен!")
+    await message.answer("Выберите действие.", reply_markup=kb_admin())
